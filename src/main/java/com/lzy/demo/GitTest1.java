a262eaa67e4f962c14a7f19e3f0caadb1c76287f
vincent.lu <lzy415337695@126.com>
2020-03-14 19:27:34 +0800

    second commit

---
+++ src/main/java/com/lzy/demo/GitTest1.java
@@ -0,0 +1,8 @@
+package com.lzy.demo;
+
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("第一行代码");
+        System.out.println("第二行代码");
+    }
+}
---
+++ src/main/java/com/lzy/demo/GitTest2.java
@@ -0,0 +1,7 @@
+package com.lzy.demo;
+
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("git第二个文件");
+    }
+}
