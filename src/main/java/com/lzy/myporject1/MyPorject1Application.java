de7dde9a4c5cf780f86cef3baabd0ca7c167dda8
vincent.lu <lzy415337695@126.com>
2020-03-14 17:15:08 +0800

    项目初始化

---
+++ src/main/java/com/lzy/myporject1/MyPorject1Application.java
@@ -0,0 +1,13 @@
+package com.lzy.myporject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPorject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPorject1Application.class, args);
+	}
+
+}
