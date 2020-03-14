80518b038f5b66b08b8a5c3e6955c36f79cf1772
vincent.lu <lzy415337695@126.com>
2020-03-14 23:17:19 +0800

    dev branch write oneLine code

--- src/main/java/com/lzy/demo/GitTest1.java
+++ src/main/java/com/lzy/demo/GitTest1.java
@@ -1,9 +1,14 @@
 package com.lzy.demo;
 
+import java.util.HashMap;
+
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("第一行代码");
         System.out.println("第二行代码");
         System.out.println("第三行代码");
+        System.out.println("dev分支写的代码");
+        HashMap<String, Integer> stringIntegerHashMap = new HashMap<>();
+
     }
 }
